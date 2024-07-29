4a734bb9e282e4655b6232368c0a55e813e4bf30
Alejandro Duque <alduk0110@gmail.com>
2024-07-29 09:51:43 -0500

    Add base Product seeder

---
+++ assesment/src/main/java/com/riwi/assesment/utils/Seeders/ProductsSeeder.java
@@ -0,0 +1,13 @@
+package com.riwi.assesment.utils.Seeders;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class ProductsSeeder {
+
+}
